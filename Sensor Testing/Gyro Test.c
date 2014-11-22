2ae8ea38d92f4f4b59b27a2140b53b078fc633a2
Izhmash <ianballou67@gmail.com>
2014-11-22 16:30:13 -0500

    updated gyro code

--- Sensor Testing/Gyro Test.c
+++ Sensor Testing/Gyro Test.c
@@ -1,15 +1,22 @@
 #pragma config(Sensor, S1,     HTGYRO,              sensorAnalogInactive)
 
+/**
+ *	This program finds the current heading with the HT Gyro Sensor
+ */
+
 #include "hitechnic-gyro.h"
 
-double gyroRaw;
-double tempW;
-double avgW;
-double angle;
-double count;
+float gyroRaw;
+float tempW;
+float avgW;
+float angle;
+float count;
+float heading;
 
 task main()
 {
+	int safetyValue = 1;  //value to protect against poor calibration
+	heading = 0;
 	gyroRaw = 0;
 	avgW = 0;
 	//angle = 0;
@@ -21,18 +28,20 @@ task main()
     avgW = 0;
     count = 0;
     //angle = 0;
-		if(abs(gyroRaw) > 15){
+		if(abs(gyroRaw) > safetyValue){
 			time1[T1] = 0;
-			while(abs(gyroRaw) > 15){
+			while(abs(gyroRaw) > safetyValue){
 				gyroRaw = HTGYROreadRot(HTGYRO);
 				tempW += gyroRaw;
 				count++;
 			}
 			avgW = (tempW / count);
-			angle = avgW*time1[T1]/1000;
+			angle = avgW*time1[T1]/1000;  //(change in angle) = (avg angular velocity) / (time in seconds)
 			time1[T1] = 0;
 		}
-		nxtDisplayCenteredBigTextLine(3, "%d", angle);
-		delay(100);
+		heading += angle;
+		angle = 0;
+		nxtDisplayCenteredBigTextLine(3, "%d", heading);
+		//delay(100);
 	}
 }
