081e0a28c6aff5404c80cb313d194b40ef4435db
Izhmash <ianballou67@gmail.com>
2015-01-22 14:39:13 -0500

    added more encoders

--- General-Testing/motorWithArrows.c
+++ General-Testing/motorWithArrows.c
@@ -11,22 +11,24 @@ task main()
 	nMotorEncoder[motor1] = 0;
 	nMotorEncoder[motor2] = 0;
 
-	while(true)
-	{
+	while (true) {
 		//nMotorEncoder[motor1] = 0;
 		//nMotorEncoder[motor2] = 0;
-		nxtDisplayCenteredBigTextLine(4, "%d", nMotorEncoder[motor1]);
+		nxtDisplayCenteredBigTextLine(2, "%d", nMotorEncoder[motor1]);
+		nxtDisplayCenteredBigTextLine(5, "%d", nMotorEncoder[motor2]);
 		while(nNxtButtonPressed == 1)
 		{
 			motor[motor1] = 100;
 			motor[motor2] = -100;
-			nxtDisplayCenteredBigTextLine(4, "%d", nMotorEncoder[motor1]);
+			nxtDisplayCenteredBigTextLine(2, "%d", nMotorEncoder[motor1]);
+			nxtDisplayCenteredBigTextLine(5, "%d", nMotorEncoder[motor2]);
 		}
 		while(nNxtButtonPressed == 2)
 		{
 			motor[motor1] = -100;
 			motor[motor2] = 100;
-			nxtDisplayCenteredBigTextLine(4, "%d", nMotorEncoder[motor1]);
+			nxtDisplayCenteredBigTextLine(2, "%d", nMotorEncoder[motor1]);
+			nxtDisplayCenteredBigTextLine(5, "%d", nMotorEncoder[motor2]);
 		}
 		motor[motor1] = 0;
 		motor[motor2] = 0;
