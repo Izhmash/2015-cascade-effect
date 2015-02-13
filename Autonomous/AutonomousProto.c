08362b0cf8b606347ce8375a868778a5b137a531
unknown <iballou@JZ0Q512.ccsuvt.org>
2015-02-13 13:36:41 -0500

    new IR

--- Autonomous/AutonomousProto.c
+++ Autonomous/AutonomousProto.c
@@ -1,7 +1,12 @@
+#pragma config(Sensor, S4,     IR,              sensorI2CCustom)
+
+#include "hitechnic-irseeker-v2.h"
 
 task main()
 {
+	tHTIRS2 irSeeker;
+	initSensor(&irSeeker, S4);
 
-
-
+	while (true) {
+	}
 }
